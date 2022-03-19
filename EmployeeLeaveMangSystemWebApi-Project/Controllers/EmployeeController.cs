d923e02d6e57813b272b51f2620772c5280d9a1a
Anupam Pratap Singh <ca43netb109@manipalprolearn.co>
2022-03-19 11:51:51 +0530

    Exception Handling Done

--- EmployeeLeaveMangSystemWebApi-Project/Controllers/EmployeeController.cs
+++ EmployeeLeaveMangSystemWebApi-Project/Controllers/EmployeeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using EmployeeLeaveMang.ServiceLayer;
 using EmployeeLeaveMang.DomainLayer.Models;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace EmployeeLeaveMangSystemWebApi_Project.Controllers
 {
@@ -29,52 +30,99 @@ namespace EmployeeLeaveMangSystemWebApi_Project.Controllers
         [HttpGet(nameof(GetEmployeeById))]
         public ActionResult GetEmployeeById(int EmpId)
         {
-            var EmployeeClass = EmployeeService.GetEmployeeById(EmpId);
-            if (EmployeeClass != null)
+            try 
             {
-                return Ok(EmployeeClass);
+                var EmployeeClass = EmployeeService.GetEmployeeById(EmpId);
+                if (EmployeeClass != null)
+                {
+                    return Ok(EmployeeClass);
+                }
             }
+            catch(Exception e) 
+            {
+                _logger.LogError("Exception Occured",e.InnerException);
+            }
+            
             return BadRequest("Not found");
         }
         #endregion
+
         #region "Add Employee"
         [HttpPost(nameof(AddEmployee))]
         public ActionResult AddEmployee(EmployeeClass EmployeeClass)
         {
-            EmployeeService.InsertEmployee(EmployeeClass);
+            try 
+            {
+                EmployeeService.InsertEmployee(EmployeeClass);
 
-            return Ok("Employee added");
+                return Ok("Employee added");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Exception Occured", e.InnerException);
+            }
+            return BadRequest("Not found");
+
         }
         #endregion
+
         #region "Update Employee Data"
         [HttpPut(nameof(UpdateEmployee))]
         public ActionResult UpdateEmployee(EmployeeClass employee)
         {
-            EmployeeService.UpdateEmployee(employee);
+            try 
+            {
+                EmployeeService.UpdateEmployee(employee);
 
-            return Ok("Employee Updated");
+                return Ok("Employee Updated");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Exception Occured", e.InnerException);
+            }
+            return BadRequest("Not found");
+
         }
         #endregion
+
         #region "Search Employee List"
         [HttpGet(nameof(GetAllEmployee))]
         public ActionResult GetAllEmployee()
         {
-            var EmployeeClass = EmployeeService.GetAllEmployee();
-            if (EmployeeClass != null)
+            try 
             {
-                return Ok(EmployeeClass);
+                var EmployeeClass = EmployeeService.GetAllEmployee();
+                if (EmployeeClass != null)
+                {
+                    return Ok(EmployeeClass);
+                }
             }
+            catch (Exception e)
+            {
+                _logger.LogError("Exception Occured", e.InnerException);
+            }
+
             return BadRequest("Not found");
         }
+
         #endregion
+
         #region "Leave Type"
         [HttpGet(nameof(GetAllLeaveType))]
         public ActionResult GetAllLeaveType()
         {
-            var LeaveDetail = EmployeeService.GetAllLeaveType();
-            if (LeaveDetail != null)
+            try 
             {
-                return Ok(LeaveDetail);
+                var LeaveDetail = EmployeeService.GetAllLeaveType();
+                if (LeaveDetail != null)
+                {
+                    return Ok(LeaveDetail);
+                }
+            }
+            
+             catch (Exception e)
+            {
+                _logger.LogError("Exception Occured", e.InnerException);
             }
             return BadRequest("Not found");
         }
@@ -83,9 +131,18 @@ namespace EmployeeLeaveMangSystemWebApi_Project.Controllers
         [HttpPost(nameof(ApplyPLeave))]
         public ActionResult ApplyPLeave(ApplyPlannedLeave applyPlannedLeave)
         {
-            EmployeeService.ApplyPL(applyPlannedLeave);
+            try 
+            {
+                EmployeeService.ApplyPL(applyPlannedLeave);
 
-            return Ok("Leave Applied Successfully");
+                return Ok("Leave Applied Successully");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Exception Occured", e.InnerException);
+            }
+            return BadRequest("Not found");
+
         }
         #endregion
         
@@ -93,9 +150,18 @@ namespace EmployeeLeaveMangSystemWebApi_Project.Controllers
         [HttpPut(nameof(CancelPlannedLeave))]
         public ActionResult CancelPlannedLeave(int EmpId)
         {
-            EmployeeService.DeletePLeave(EmpId);
+            try 
+            {
+                EmployeeService.DeletePLeave(EmpId);
 
-            return Ok("Leave Cancelled");
+                return Ok("Leave Cancelled");
+
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Exception Occured", e.InnerException);
+            }
+            return BadRequest("Not found");
         }
         #endregion
     }
